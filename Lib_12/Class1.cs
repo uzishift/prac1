056c4d49012ce94cbdcb73e89d42288d4c991411
uzishift <uzishift@DESKTOP-6B4450E>
2024-10-02 17:32:49 +0300

    Добавьте файлы проекта.

---
+++ Lib_12/Class1.cs
@@ -0,0 +1,44 @@
+
+namespace Lib_12
+{
+    public class ClassSum
+    {
+        /// <summary>
+        /// Метод для генерации случайных значений
+        /// </summary>
+        /// <param name="quant"></param>
+        /// <returns>Случайное число от 0 до n</returns>
+        public static string GenRandom(string quant)
+        {            
+            int quantConv = Convert.ToInt32(quant);            
+            Random random = new Random();
+            string rowNumber = "";
+            for (int i = 0; i < quantConv; i++)
+            {
+                int num = random.Next(quantConv);
+                rowNumber += num.ToString() + " ";
+            }
+            return rowNumber;
+        }
+        /// <summary>
+        /// Метод для вычисления суммы
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns>Сумма чисел из tbNumbers</returns>
+        public static int Summa(string row)
+        {
+            int sum = 0;
+            for (int i = 0; i < row.Length; i++)
+            {
+                string numberString = "";
+                while (Int32.TryParse(row[i].ToString(), out int temp))
+                {
+                    numberString += temp.ToString();
+                    i++;
+                }               
+                sum += Convert.ToInt32(numberString);
+            }
+            return sum;
+        }
+    }
+}
---
+++ prac1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_12;
+
+namespace prac1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Метод инициализации элементов
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Выход из приложения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// Вывод информации о разработчике и приложении
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Демьяхин Роман ИСП-31 12 Вариант.\nНайти сумму n целых, случайных чисел, распределенных в диапазоне от 0 до n. \r\nВывести на экран на одной строке сгенерированные числа, на другой строке результат", "Информация");
+        }
+        /// <summary>
+        /// Генерация случайных значений и вывод в TextBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            int n;
+            if (int.TryParse(tbNumbers.Text, out n))
+            {
+                tbRandom.Text = ClassSum.GenRandom(n.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное целое число.", "Ошибка ввода");
+            }
+        }
+        /// <summary>
+        /// Подсчет результата и вывод в TextBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbRandom.Text != "")
+            {
+                int sum = ClassSum.Summa(tbRandom.Text);
+                tbRez.Text = sum.ToString();
+
+            }
+            else MessageBox.Show("Поле пустое", "Ошибка");
+        }
+    }
+}
